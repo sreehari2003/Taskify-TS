ee52b12a04ed92cb92c114c444a2052855c13b55
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-09-03 18:25:48 +0530

    feat: added draggable task

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,38 +1,62 @@
-import React from 'react';
-import {Todo} from "./modal"
-import { AiFillEdit,AiFillDelete } from "react-icons/ai";
+import React from "react";
+import { Todo } from "./modal";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import { Draggable } from "react-beautiful-dnd";
 
-
-interface props{
-    todo: Todo;
-    todos:Todo[];
-    setTodo:React.Dispatch<React.SetStateAction<string>>;
-    // key:number;
+interface props {
+  index: number;
+  todo: Todo;
+  todos: Todo[];
+  setTodo?: React.Dispatch<React.SetStateAction<string>>;
+  completed?: boolean;
+  setTodos?: (el: Todo[]) => void;
+  // key:number;
 }
-const SingleTodo:React.FC<props> = ({todo,todos,setTodo}) => {
-    const handleDone = (id:number)=>{
-        console.log("hello");
-    }
+const SingleTodo: React.FC<props> = ({
+  todo,
+  todos,
+  setTodos,
+  index,
+  completed = false,
+}) => {
+  console.log(todos);
+  const handleDone = () => {
+    const newState = todos.map((el) => {
+      if (el.id === todo.id) {
+        return { ...todo, isDone: true };
+      }
+      return el;
+    });
+    setTodos(newState);
+  };
   return (
-      <form  className="todos__single">
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided) => (
+        <form
+          className="todos__single"
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
           <span className="todos__single--text">{todo.todo}</span>
-
-          <div>
-              <span className="icon">
-              <AiFillEdit/>
+          {!completed && (
+            <div>
+              {/* <span className="icon">
+                <AiFillEdit />
               </span>
               <span className="icon">
-                  <AiFillDelete/>
+                <AiFillDelete />
+              </span> */}
+              <span className="icon" onClick={handleDone}>
+                <MdDone />
               </span>
-              {/* didnt understood */}
-              <span className="icon" onClick={()=>handleDone}>
-                   <MdDone/>
-              </span>
-          </div>
-      </form>
-  )
-
+            </div>
+          )}
+        </form>
+      )}
+    </Draggable>
+  );
 };
 
 export default SingleTodo;
